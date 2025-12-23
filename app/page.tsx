f7b036bc46405b22d2bf4c849f5b2970a1d17a5a
voidumair <umairsarwar4254@gmail.com>
2025-12-23 18:35:45 +0500

    Dashboard Complete

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Heading from "@/components/shared/Heading";
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center bg-background font-dm-sans">
-      <div className="w-full max-w-[1050px] h-full flex flex-col items-center pt-6 pb-12 px-8 gap-8">
+      <div className="w-full max-w-[1050px] h-full flex flex-col items-center pt-6 pb-12 md:px-8 px-3 gap-8">
         <Heading
           title="Customize Your Appointment"
           description="You can add extra services to your selected treatment"
